e962981849707cad50d9b44daf25af352ce60b3d
SashaPlavi <sasha.bingulac@gmail.com>
2020-06-29 09:03:03 +0200

    Updates

---
+++ precache-manifest.f9a815eff5c749a5db45afafaf18178f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94ae2c65540935841b1ba9f77b002a62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a62795ebbb8193d9be5",
+    "url": "/static/css/main.dd51626c.chunk.css"
+  },
+  {
+    "revision": "f4938a7dd358b03ae2f6",
+    "url": "/static/js/2.47979ae4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.47979ae4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a62795ebbb8193d9be5",
+    "url": "/static/js/main.8050bd83.chunk.js"
+  },
+  {
+    "revision": "a98384c36e9903635e0d",
+    "url": "/static/js/runtime-main.4357a549.js"
+  }
+]);
